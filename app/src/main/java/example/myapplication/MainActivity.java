82462a754fe8870c765bfdf6ee0f556e206fe004
tylooNB <lirunhuajack@163.com>
2019-09-26 21:29:59 +0800

    测试

--- app/src/main/java/example/myapplication/MainActivity.java
+++ app/src/main/java/example/myapplication/MainActivity.java
@@ -35,7 +35,6 @@ public class MainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
